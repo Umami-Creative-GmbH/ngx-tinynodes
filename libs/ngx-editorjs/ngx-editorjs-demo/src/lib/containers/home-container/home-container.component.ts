da177e45d01f67a4c75793b00618049937e72cab
Kai <kai@umami-creative.de>
2024-05-23 12:02:04 +0200

    removed demo to cleanup

--- libs/ngx-editorjs/ngx-editorjs-demo/src/lib/containers/home-container/home-container.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-editorjs-demo-home',
-  templateUrl: 'home-container.component.html',
-  styleUrls: ['home-container.component.scss']
-})
-export class NgxEditorJSDemoHomeComponent {}
